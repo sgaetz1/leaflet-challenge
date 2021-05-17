ef6ab3a74f32f93cbe68be86d3bc9c70dbee2074
Steven Gaetz <gaetz025@umn.edu>
2021-05-17 12:01:34 -0500

    made some double checking

--- static/js/logic.js
+++ static/js/logic.js
@@ -40,123 +40,123 @@ d3.json(geoData).then(function(data) {
 
 
 
-  // Adding tile layer
-  var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/streets-v11",
-    accessToken: API_KEY
-  });
+    // Adding tile layer
+    var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+      tileSize: 512,
+      maxZoom: 18,
+      zoomOffset: -1,
+      id: "mapbox/streets-v11",
+      accessToken: API_KEY
+    });
 
-  var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "dark-v10",
-    accessToken: API_KEY
-  });
+    var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      maxZoom: 18,
+      id: "dark-v10",
+      accessToken: API_KEY
+    });
 
-  var OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-	maxZoom: 17,
-	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-  });
+    var OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    maxZoom: 17,
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+    });
 
-  var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-  });
+    var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    });
 
-  var baseMaps = {
-    "Street Map": streetmap,
-    "Dark Map": darkmap,
-    "Topographic": OpenTopoMap,
-    "World Imagery": Esri_WorldImagery
-  };
+    var baseMaps = {
+      "Street Map": streetmap,
+      "Dark Map": darkmap,
+      "Topographic": OpenTopoMap,
+      "Satellite": Esri_WorldImagery
+    };
 
-  // saving earthquake data to array
-  var earthquakeMarkers = [];
+    // saving earthquake data to array
+    var earthquakeMarkers = [];
 
-  for (var i = 0; i < features.length; i++) {
-    var coordinates = []
-    coordinates.push(features[i].geometry.coordinates[1], features[i].geometry.coordinates[0]);
-    
-    
-    var circle =  L.circle(coordinates, {
-        stroke: false,
-        fillOpacity: 0.75,
-        color: "#000000",
-        fillColor: markerColor(features[i].geometry.coordinates[2]),
-        radius: markerSize(features[i].properties.mag)
-        })
-    
-    circle.bindPopup("<h3>" + features[i].properties.place + 
-      "</h3><hr><h4>Magnitude: " +  (features[i].properties.mag) + "</h4>");  
+    for (var i = 0; i < features.length; i++) {
+      var coordinates = []
+      coordinates.push(features[i].geometry.coordinates[1], features[i].geometry.coordinates[0]);
       
-    earthquakeMarkers.push(circle);
-  }
-  
-  // saving tectonic plate data to array
-  var tectonicLines = [];
-
-  for (var i = 0; i < tectonicFeatures.length; i++) {
-    var lineCoordinates = [];
-    var coordinates = tectonicFeatures[i].geometry.coordinates;
-
-    // switching the latitude and longitude
-    for (var j = 0; j < coordinates.length; j++) {
-      lineCoordinates.push([coordinates[j][1], coordinates[j][0]]);
+      
+      var circle =  L.circle(coordinates, {
+          stroke: false,
+          fillOpacity: 0.75,
+          color: "#000000",
+          fillColor: markerColor(features[i].geometry.coordinates[2]),
+          radius: markerSize(features[i].properties.mag)
+          })
+      
+      circle.bindPopup("<h3>" + features[i].properties.place + 
+        "</h3><hr><h4>Magnitude: " +  (features[i].properties.mag) + "</h4>");  
+        
+      earthquakeMarkers.push(circle);
     }
-    // console.log(lineCoordinates);
-    var line = L.polyline(lineCoordinates, {
-      color: "#FFD700"
-    })
-  
-    tectonicLines.push(line);
-  }
-  
+    
+    // saving tectonic plate data to array
+    var tectonicLines = [];
 
-  var earthquakes = L.layerGroup(earthquakeMarkers);
-  var tectonicPlates = L.layerGroup(tectonicLines);
+    for (var i = 0; i < tectonicFeatures.length; i++) {
+      var lineCoordinates = [];
+      var coordinates = tectonicFeatures[i].geometry.coordinates;
 
-  var overlayMaps = {
-    "Earthquakes": earthquakes,
-    "Tectonic Plates": tectonicPlates
-  };
-
-  // Create map object
-  var myMap = L.map("mapid", {
-    center: [37.09, -95.71],
-    zoom: 3,
-    layers: [streetmap, earthquakes]
-  });
-
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(myMap);
-
-
-  // set up legend
-  var legend = L.control({position: 'bottomright'});
-
-  legend.onAdd = function () {
-
-    var div = L.DomUtil.create('div', 'info legend');
-      grades = [-10, 10, 30, 50, 70, 90];
-      labels = [];
-
-      for (var i = 0; i < grades.length; i ++) {
-        div.innerHTML +=
-          labels.push('<i style="background:' + markerColor(grades[i] + 1) + '"></i>' +
-          grades[i] + (grades[i +1] ? '&ndash;' + grades[i + 1] + '<br>' : '+'));
+      // switching the latitude and longitude
+      for (var j = 0; j < coordinates.length; j++) {
+        lineCoordinates.push([coordinates[j][1], coordinates[j][0]]);
       }
-      
-      div.innerHTML = labels.join('');
-      return div;
-      
-  };
+      // console.log(lineCoordinates);
+      var line = L.polyline(lineCoordinates, {
+        color: "#FFD700"
+      })
+    
+      tectonicLines.push(line);
+    }
+    
 
-  // add legend to the map
-  legend.addTo(myMap);
-})
+    var earthquakes = L.layerGroup(earthquakeMarkers);
+    var tectonicPlates = L.layerGroup(tectonicLines);
+
+    var overlayMaps = {
+      "Earthquakes": earthquakes,
+      "Tectonic Plates": tectonicPlates
+    };
+
+    // Create map object
+    var myMap = L.map("mapid", {
+      center: [37.09, -95.71],
+      zoom: 3,
+      layers: [streetmap, earthquakes]
+    });
+
+    L.control.layers(baseMaps, overlayMaps, {
+      collapsed: false
+    }).addTo(myMap);
+
+
+    // set up legend
+    var legend = L.control({position: 'bottomright'});
+
+    legend.onAdd = function () {
+
+      var div = L.DomUtil.create('div', 'info legend');
+        grades = [-10, 10, 30, 50, 70, 90];
+        labels = [];
+
+        for (var i = 0; i < grades.length; i ++) {
+          div.innerHTML +=
+            labels.push('<i style="background:' + markerColor(grades[i] + 1) + '"></i>' +
+            grades[i] + (grades[i +1] ? '&ndash;' + grades[i + 1] + '<br>' : '+'));
+        }
+        
+        div.innerHTML = labels.join('');
+        return div;
+        
+    };
+
+    // add legend to the map
+    legend.addTo(myMap);
+  })
 })  
   
